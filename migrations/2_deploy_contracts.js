c99e02f3bc0d4ca87e3d05cd3522c2c73ce296be
lluisemper <lluisemper@gmail.com>
2020-09-07 10:59:56 +0100

    set up and contracts

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,13 @@
+const Token = artifacts.require("Token");
+
+const EthSwap = artifacts.require("EthSwap");
+
+module.exports = async function(deployer) {
+  await deployer.deploy(EthSwap);
+  const ethSwap = await EthSwap.deployed();
+
+  await deployer.deploy(Token);
+  const token = await Token.deployed();
+
+  await token.transfer(ethSwap.address, "1000000000000000000000000");
+};
